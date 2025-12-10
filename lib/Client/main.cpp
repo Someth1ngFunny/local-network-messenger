b35557a6e47c8adc51bb9f4284967059aa152a2c
Perevyazkin Denis <135735261+Someth1ngFunny@users.noreply.github.com>
2025-12-10 20:38:57 +0300

    v0.1.1
    - Link asio dependecie on Windows
    - Delete dev logs

--- lib/Client/main.cpp
+++ lib/Client/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 {
   try
   {
-    
+    asio::io_context io_context;
   }
   catch (const std::exception& error)
   {
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <asio.hpp>
 
 int main() {
+
+  try
+  {
+    asio::io_context io_context;
+  } 
+  catch (const std::exception& e) 
+  {
+
+  }
 
   return 0;
 }
