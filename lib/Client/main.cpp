ff16cd03c53d32459e916322abd22197e3b46b57
Perevyazkin Denis <135735261+Someth1ngFunny@users.noreply.github.com>
2025-12-10 19:56:14 +0300

    v0.1
    Create project build system
    
    - Add ways to build project via
    1) Only CMake (via FetchContent)
    2) Only Python (via conan package-manager)
    * Support for single- and multi- config build systems

---
+++ lib/Client/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <asio.hpp>
+
+int main(int argc, char* argv[]) 
+{
+  try
+  {
+    
+  }
+  catch (const std::exception& error)
+  {
+    std::cout << error.what() << std::endl;
+  }
+}
