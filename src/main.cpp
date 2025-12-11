5892d340c7e5d5f06690de72cc208d0420630877
Perevyazkin Denis <135735261+Someth1ngFunny@users.noreply.github.com>
2025-12-11 22:14:39 +0300

    v0.1.2
    Change project CMake structure
    
    - Add fundament for future core object Logic
    - Add fundament for future pars object Logic
    - Add simple raw main.cpp version

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
-#include <asio.hpp>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <format>
+#include <logic.hpp>
+#include <lexparser.hpp>
 
-int main() {
+Logic GLOBAL_STATE;
+
+std::unordered_map<std::string, void (*)(const std::vector<std::string>&)> command_handler;
+
+void exit(const std::vector<std::string> &lexems) 
+{
+  throw std::runtime_error("exit");
+}
+
+void handle_uknown_command(const std::string &command) 
+{
+  std::cout << std::format("Uknown command: \"{}\"", command) << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+
+  command_handler["exit"] = exit;
 
   try
   {
-    asio::io_context io_context;
+    std::string str_line;
+    
+    auto welcome = [&str_line]()
+    {
+      std::cout << ">> ";
+      std::getline(std::cin, str_line);
+      return true;
+    };
+
+    while (welcome()) 
+    {
+      std::stringstream stream_line(str_line);
+      
+      std::vector<std::string> lexems;
+      std::string lexem;
+      while (stream_line >> lexem)
+      {
+        lexems.push_back(lexem);
+      }
+
+      if (lexems.empty()) 
+      { 
+        continue; 
+      }
+      if (!command_handler.contains(lexems[0])) 
+      {
+        handle_uknown_command(lexems[0]);
+        continue;
+      }
+
+      (command_handler[lexems[0]])(lexems);
+    }
+
   } 
   catch (const std::exception& e) 
   {
